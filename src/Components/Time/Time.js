b06adda26cfadcca2f24b9c785c103b3456219bd
Martin Burton <martin_burton@hotmail.com>
2022-04-02 15:42:32 +0100

    Add clock functionality

--- src/Components/Time/Time.js
+++ src/Components/Time/Time.js
@@ -1,9 +1,19 @@
+import { useState, useEffect } from 'react';
 import React from 'react';
 
 const Time = () => {
+  const [clockState, setClockState] = useState();
+
+  useEffect(() => {
+    setInterval(() => {
+      const date = new Date();
+      setClockState(date.toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit', hour12: true }));
+    }, 1000)
+  });
+
   return(
     <div style={{marginLeft: 'auto', paddingRight: '10px'}}>
-      <h4>Time</h4>
+      <h3>{clockState}</h3>
     </div>
   );
 };
